0750a11bca0239d8590a53450f0d9ad5dd019525
Paris Pray <paris.pray@gmail.com>
2020-09-02 20:53:51 -0600

    Revised CharacterController

--- Practice/Assets/Scripts/CharacterMover.cs
+++ Practice/Assets/Scripts/CharacterMover.cs
@@ -5,48 +5,35 @@ using UnityEngine.UI;
 [RequireComponent(typeof(CharacterController))]
 public class CharacterMover : MonoBehaviour
 {
-    private CharacterController controller;
-    private Vector3 movement;
-    public float gravity = 9.81f;
-    public float moveSpeed = 3f;
-    public float fastMoveSpeed;
-    public float jumpForce = 10f;
-    public int jumpCountMax;
-    public float rotateSpeed = 3f;
-    private Vector3 rotateMovement;
+  private CharacterController controller;
+  private Vector3 movement;
+  public float moveSpeed = 5f, gravity = -9.81f, jumpForce = 10f, rotateSpeed = 30f;
+  private float yVar;
+  private void Start()
+  {
+    controller = GetComponent<CharacterController>();
+  }
+  private void Update()
+  {
+    var vInput = Input.GetAxis("Vertical");
     
-    // Start is called before the first frame update
-    void Start()
+    movement.Set(moveSpeed*vInput, gravity, 0);
+    movement = transform.TransformDirection(movement);
+    controller.Move(movement * Time.deltaTime);
+    var hInput = Input.GetAxis("Horizontal");
+    transform.Rotate(0,hInput,0);
+    
+    yVar += gravity;
+
+    
+    if (controller.isGrounded && movement.y <0)
     {
-        controller = GetComponent<CharacterController>();
+      yVar = -1f;
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    if (Input.GetKeyDown(KeyCode.Space))
     {
-        transform.Rotate(rotateMovement);
-        movement.x = moveSpeed;
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            movement.x = -moveSpeed;
-        }
-        
-        if (Input.GetButtonDown("Jump"))
-        {
-            movement.y = jumpForce;
-        }
-
-        if (controller.isGrounded)
-        {
-            movement.y = 0;
-        }
-        else
-        {
-            movement.y -= gravity;
-        }
-
-        movement = transform.TransformDirection(movement);
-        controller.Move(movement*Time.deltaTime);
+      yVar += Mathf.Sqrt(jumpForce * gravity);
     }
+  }
 }

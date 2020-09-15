fc01ad4b826562e1a7949b7c994e288581e2d002
Paris Pray <paris.pray@gmail.com>
2020-09-14 21:38:53 -0600

    9/14 class

--- Practice/Assets/Scripts/Vector3Data.cs
+++ Practice/Assets/Scripts/Vector3Data.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[CreateAssetMenu]
 public class Vector3Data : ScriptableObject
 {
     public Vector3 value;
+
+    public void SetValueFromTrasform(Vector3 obj)
+    {
+        value = obj;
+    }
 }

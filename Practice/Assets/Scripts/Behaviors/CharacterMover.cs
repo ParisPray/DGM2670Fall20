b06c3b7b8bc7b94f2e21e8b5a038c6f3500146fb
Paris Pray <paris.pray@gmail.com>
2020-10-12 23:11:00 -0600

    Added and studied AI Behaviour

--- Practice/Assets/Scripts/Behaviors/CharacterMover.cs
+++ Practice/Assets/Scripts/Behaviors/CharacterMover.cs
@@ -7,62 +7,105 @@ using UnityEngine.UI;
 public class CharacterMover : MonoBehaviour
 {
   private CharacterController controller;
-  private Vector3 movement;
-  public float gravity = -9.81f, jumpForce = 10f, rotateSpeed = 30f;
-  private float yVar;
+    private Vector3 movement;
 
-  public IntData playerJumpCount;
-  private int jumpCount;
-  public Vector3Data currentSpawnPoint;
-  public floatData moveSpeed, normalSpeed, fastSpeed;
-  private void Start()
-  {
-    moveSpeed = normalSpeed;
-    controller = GetComponent<CharacterController>();
-  }
+    public float rotateSpeed = 30f, gravity = -9.81f, jumpForce = 10f;
+    private float yVar;
 
-  private void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.LeftShift))
+    public floatData normalSpeed, fastSpeed;
+    private floatData moveSpeed;
+    private bool canMove = true;
+    
+    public IntData playerJumpCount;
+    private int jumpCount;
+
+    private void Start() 
     {
-      moveSpeed = fastSpeed;
+        moveSpeed = normalSpeed;
+        controller = GetComponent<CharacterController>();
+        StartCoroutine(Move());
     }
 
-    if (Input.GetKeyUp(KeyCode.LeftShift))
+    private readonly WaitForFixedUpdate wffu = new WaitForFixedUpdate();
+    private IEnumerator Move()
     {
-      moveSpeed = normalSpeed;
+        canMove = true;
+        while (canMove)
+        {
+            yield return wffu;
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                moveSpeed = fastSpeed;
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                moveSpeed = normalSpeed;
+            }
+        
+            var vInput = Input.GetAxis("Vertical")*moveSpeed.value;
+            
+            movement.Set(vInput,yVar,0);
+        
+            var hInput = Input.GetAxis("Horizontal")*Time.deltaTime*rotateSpeed;
+            transform.Rotate(0,hInput,0);
+
+            yVar += gravity*Time.deltaTime;
+
+            if (controller.isGrounded && movement.y < 0)
+            {
+                yVar = -1f;
+                jumpCount = 0;
+            }
+
+            if (Input.GetButtonDown("Jump") && jumpCount < playerJumpCount.value)
+            {
+                yVar = jumpForce;
+                jumpCount++;
+            }
+        
+            movement = transform.TransformDirection(movement);
+            controller.Move((movement) * Time.deltaTime);
+        }
+    }
+    private IEnumerator KnockBack (ControllerColliderHit hit, Rigidbody body)
+    {
+        canMove = false;
+        var i = 2f;
+       
+        movement = -hit.moveDirection;
+        movement.y = -1;
+        while (i > 0)
+        {
+            yield return wffu;
+            i -= 0.1f;
+            controller.Move((movement) * Time.deltaTime);
+            
+            var pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+            var forces = pushDir * pushPower;
+            body.AddForce(forces);
+        }
+        movement = Vector3.zero;
+        StartCoroutine(Move());
     }
     
-    var vInput = Input.GetAxis("Vertical") * moveSpeed.value;
+    public float pushPower = 10.0f;
+    private CharacterController characterController;
 
-    movement.Set(vInput, yVar, 0);
-    movement = transform.TransformDirection(movement);
-
-    controller.Move(movement * Time.deltaTime);
-    var hInput = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
-
-    transform.Rotate(0, hInput, 0);
-
-    yVar += gravity * Time.deltaTime;
-
-
-    if (controller.isGrounded && movement.y < 0)
+    void OnControllerColliderHit(ControllerColliderHit hit)
     {
-      yVar = -1f;
-      jumpCount = 0;
-    }
-
-    if (Input.GetButtonDown("Jump")&& jumpCount < playerJumpCount.value)
-    {
-      yVar += jumpForce;
-      jumpCount++;
-    }
+        var body = hit.collider.attachedRigidbody;
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
     
-    void OnEnable()
-    {
-      transform.position = currentSpawnPoint.value;
+        if (hit.moveDirection.y < -0.3)
+        {
+            return;
+        }
+       
+        StartCoroutine(KnockBack(hit, body));
     }
-    
-  }
 }
 

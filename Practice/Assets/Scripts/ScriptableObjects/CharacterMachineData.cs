1befb65876df6e00b6fe14741f9628c5bee2ebb8
Paris Pray <paris.pray@gmail.com>
2020-11-03 16:44:49 -0700

    Fixed Statemachine, found error. CanHunt code works now too.

--- Practice/Assets/Scripts/ScriptableObjects/CharacterMachineData.cs
+++ Practice/Assets/Scripts/ScriptableObjects/CharacterMachineData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-
+[CreateAssetMenu]
 public class CharacterMachineData : ScriptableObject
 {
     public enum characterStates

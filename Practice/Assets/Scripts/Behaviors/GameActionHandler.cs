23a7337b24f0c02f5efe90528857450b0419c089
Paris Pray <paris.pray@gmail.com>
2020-11-04 23:17:17 -0700

    Worked with GameActions

--- Practice/Assets/Scripts/Behaviors/GameActionHandler.cs
+++ Practice/Assets/Scripts/Behaviors/GameActionHandler.cs
@@ -7,14 +7,22 @@ using UnityEngine.Events;
 
 public class GameActionHandler : MonoBehaviour
 {
+    
     public GameAction gameAction;
     public UnityEvent handlerEvent;
     public float holdTime = 3f;
+    public bool stopHandling;
+
+    public bool StopHandling
+    {
+        get => stopHandling;
+        set => stopHandling = value;
+    }
+
     private void Start()
     {
         gameAction.action += ActionHandler;  
     }
-
     private void ActionHandler()
     {
         Invoke(nameof(OnActionHandler), holdTime);
@@ -22,6 +30,8 @@ public class GameActionHandler : MonoBehaviour
     
     private void OnActionHandler()
     {
+        if (StopHandling) return;
+        
         handlerEvent.Invoke();
     }
 }
--- Practice/Assets/Scripts/Behaviors/playerBehaviour.cs
+++ Practice/Assets/Scripts/Behaviors/playerBehaviour.cs
@@ -5,109 +5,109 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class playerBehaviour : MonoBehaviour
 {
-    private CharacterController controller;
-        private Vector3 movement;
-        public Vector3 lookDirection, finalDirection;
-        private float yVar;
-        private bool canMove = true, canJump = true, canSprint = true;
-        private float gravity = -9.81f;
-        public floatData normalSpeed, fastSpeed, jumpForce;
-        private floatData currentSpeed;
-        public IntData playerJumpMax;
-        private int jumpCount;
-        public float pushForce;
-        public float waitTime;
-        private WaitForFixedUpdate wffu = new WaitForFixedUpdate();
-        private WaitForSeconds wfs;
-    
-        private void Start()
+      private CharacterController controller;
+    private Vector3 movement;
+    public Vector3 lookDirection, finalDirection;
+    private float yVar;
+    private bool canMove = true, canJump = true, canSprint = true;
+    private float gravity = -9.81f;
+    public floatData normalSpeed, fastSpeed, jumpForce;
+    private floatData currentSpeed;
+    public IntData playerJumpMax;
+    private int jumpCount;
+    public float pushForce;
+    public float waitTime;
+    private WaitForFixedUpdate wffu = new WaitForFixedUpdate();
+    private WaitForSeconds wfs;
+
+    private void Start()
+    {
+        currentSpeed = normalSpeed;
+        controller = GetComponent<CharacterController>();
+        StartCoroutine(Move());
+        wfs = new WaitForSeconds(waitTime);
+    }
+
+    private IEnumerator Move()
+    {
+        canMove = true;
+        while (canMove)
         {
-            currentSpeed = normalSpeed;
-            controller = GetComponent<CharacterController>();
-            StartCoroutine(Move());
-            wfs = new WaitForSeconds(waitTime);
+            yield return wffu;
+            yVar += gravity * Time.deltaTime;
+            var vInput = Input.GetAxis("Vertical") * currentSpeed.value * Time.deltaTime;
+            var hInput = Input.GetAxis("Horizontal") * currentSpeed.value * Time.deltaTime;
+            if (controller.isGrounded && movement.y < 0)
+            {
+                yVar = -1;
+                jumpCount = 0;
+            }
+
+            if (Input.GetButtonDown("Jump") && jumpCount < playerJumpMax.value)
+            {
+                yVar = jumpForce.value;
+                jumpCount++;
+            }
+            
+
+            if (Input.GetButtonDown("Fire3"))
+            {
+                currentSpeed = fastSpeed;
+            }
+            else if (Input.GetButtonUp("Fire3"))
+            {
+                currentSpeed = normalSpeed;
+            }
+
+            lookDirection.Set(hInput, 0, vInput);
+            if (hInput > 0.05f || hInput < -0.05f || vInput > 0.05f || vInput < -0.05f)
+            {
+                finalDirection.Set(lookDirection.x, 0, lookDirection.z);
+            }
+
+            if (lookDirection != Vector3.zero)
+            {
+                transform.rotation = Quaternion.LookRotation(finalDirection);
+            }
+
+            movement.Set(hInput, yVar, vInput);
+            controller.Move(movement);
         }
-    
-        private IEnumerator Move()
+    }
+
+    private IEnumerator KnockBack(ControllerColliderHit hit, Rigidbody rBody)
+    {
+        canMove = false;
+        var pushDistance = 2f;
+        movement = -hit.moveDirection;
+        movement.y = -1;
+        while (pushDistance > 0)
         {
-            canMove = true;
-            while (canMove)
-            {
-                yield return wffu;
-                yVar += gravity * Time.deltaTime;
-                var vInput = Input.GetAxis("Vertical") * currentSpeed.value * Time.deltaTime;
-                var hInput = Input.GetAxis("Horizontal") * currentSpeed.value * Time.deltaTime;
-                if (controller.isGrounded && movement.y < 0)
-                {
-                    yVar = -1;
-                    jumpCount = 0;
-                }
-    
-                if (Input.GetButtonDown("Jump") && jumpCount < playerJumpMax.value)
-                {
-                    yVar = jumpForce.value;
-                    jumpCount++;
-                }
-                
-    
-                if (Input.GetButtonDown("Fire3"))
-                {
-                    currentSpeed = fastSpeed;
-                }
-                else if (Input.GetButtonUp("Fire3"))
-                {
-                    currentSpeed = normalSpeed;
-                }
-    
-                lookDirection.Set(hInput, 0, vInput);
-                if (hInput > 0.05f || hInput < -0.05f || vInput > 0.05f || vInput < -0.05f)
-                {
-                    finalDirection.Set(lookDirection.x, 0, lookDirection.z);
-                }
-    
-                if (lookDirection != Vector3.zero)
-                {
-                    transform.rotation = Quaternion.LookRotation(finalDirection);
-                }
-    
-                movement.Set(hInput, yVar, vInput);
-                controller.Move(movement);
-            }
+            yield return wffu;
+            pushDistance -= 0.1f;
+            controller.Move(movement * Time.deltaTime);
+            var pushDirection = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+            var forces = pushDirection * pushForce;
+            rBody.AddForce(forces);
         }
-    
-        private IEnumerator KnockBack(ControllerColliderHit hit, Rigidbody rBody)
+
+        movement = Vector3.zero;
+        StartCoroutine(Move());
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        var rbody = hit.collider.attachedRigidbody;
+        if (rbody == null || rbody.isKinematic)
         {
-            canMove = false;
-            var pushDistance = 2f;
-            movement = -hit.moveDirection;
-            movement.y = -1;
-            while (pushDistance > 0)
-            {
-                yield return wffu;
-                pushDistance -= 0.1f;
-                controller.Move(movement * Time.deltaTime);
-                var pushDirection = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-                var forces = pushDirection * pushForce;
-                rBody.AddForce(forces);
-            }
-    
-            movement = Vector3.zero;
-            StartCoroutine(Move());
+            return;
         }
-    
-        private void OnControllerColliderHit(ControllerColliderHit hit)
+
+        if (hit.moveDirection.y < -0.3)
         {
-            var rbody = hit.collider.attachedRigidbody;
-            if (rbody == null || rbody.isKinematic)
-            {
-                return;
-            }
-    
-            if (hit.moveDirection.y < -0.3)
-            {
-                return;
-            }
-    
-            StartCoroutine(KnockBack(hit, rbody));
+            return;
         }
+
+        StartCoroutine(KnockBack(hit, rbody));
+    }
 }

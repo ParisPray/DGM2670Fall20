95597b2ffc28ee2112f76fff973200a374334599
Paris Pray <paris.pray@gmail.com>
2020-11-09 21:52:44 -0700

    Studying classwork code

--- Practice/Assets/Scripts/Behaviors/AIBehavior.cs
+++ Practice/Assets/Scripts/Behaviors/AIBehavior.cs
@@ -50,6 +50,4 @@ public class AIBehavior : MonoBehaviour
             i = (i + 1) % patrolPoints.Count;
         }
     }
-   
-    }
-
+}
---
+++ Practice/Assets/Scripts/ScriptableObjects/NavAgentBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class NavAgentBehaviour : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+}

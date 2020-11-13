8ad6a6d9daa79c595e039a76e9c9b0b0dd81b0bd
Paris Pray <paris.pray@gmail.com>
2020-11-12 19:44:43 -0700

    Testing Variable Spawn code

---
+++ Practice/Assets/Scripts/Behaviors/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+   
+    public GameObject spawnObject;
+ 
+    void Update ()
+    {
+        StartCoroutine(Spawn());
+    }
+    
+    IEnumerator Spawn ()
+    {
+        yield return new WaitForSeconds (Random.Range (1, 600));
+        Instantiate(spawnObject);
+        Debug.Log("Object Spawned");
+    }
+}

c9fe9ea4452268878d5b3ab2935c376d4ea3dca7
Paris Pray <paris.pray@gmail.com>
2020-11-19 17:03:00 -0700

    AI Brain Base Practice

---
+++ Practice/Assets/Scripts/Behaviors/AIBrainBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu]
+public class AIBrainBase : ScriptableObject
+{
+    public float health;
+    public float speed;
+    public Mesh art;
+    private Vector3 position;
+    public Vector3Data location;
+
+    public virtual void Navigate(NavMeshAgent agent)
+    {
+        position = location.value;
+        //destination
+        //Make agents to different things
+    }
+}
